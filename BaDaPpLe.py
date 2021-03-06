3d79fd84b9e61eae51248a445c4bdb4f767d2cb4
Shiraishia20 <64631783+Shiraishia20@users.noreply.github.com>
2021-03-06 21:01:57 +0700

    Update BaDaPpLe.py

--- BaDaPpLe.py
+++ BaDaPpLe.py
@@ -33,7 +33,7 @@ import discord
 import os
 from discord.ext import commands
 TOKEN = os.getenv("TOKEN")
-PREFIX = '!'
+PREFIX = 'b.'
 INTENTS = discord.Intents.default()
 bot = commands.Bot(command_prefix=PREFIX, intents=INTENTS)
 
